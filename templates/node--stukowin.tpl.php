13b3769f48307243026434ad4f43119429d767a5
TheJake123 <zebra_streif@hotmail.com>
2014-07-08 19:48:24 +0200

    -Created template

--- templates/node--stukowin.tpl.php
+++ templates/node--stukowin.tpl.php
@@ -1,51 +1,65 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*
+ * To change this license header, choose License Headers in Project Properties. To change this template file, choose Tools | Templates and open the template in the editor.
  */
-
 ?>
-<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+<div id="node-<?php print $node->nid; ?>"
+	class="<?php print $classes; ?> clearfix" <?php print $attributes; ?>>
 
   <?php print $user_picture; ?>
 
   <?php print render($title_prefix); ?>
   <?php if (!$page): ?>
-    <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+    <h2 <?php print $title_attributes; ?>>
+		<a href="<?php print $node_url; ?>"><?php print $title; ?></a>
+	</h2>
   <?php endif; ?>
   <?php print render($title_suffix); ?>
-
-  <?php if ($display_submitted): ?>
-    <div class="submitted">
-      <?php print $submitted; ?>
-    </div>
-  <?php endif; ?>
-
-  <div class="content"<?php print $content_attributes; ?>>
-    <?php	
-      // We hide the comments and links now so that we can render them later.
-      hide($content['comments']);
-      hide($content['links']);
-      //print render($content);
-    ?>
-  </div>
-
-  <?php print render($content['links']); ?>
-  <?php print render($content['comments']); ?>
-  
+  <?php if (! empty ( $content ['lvtypname'] )) {
+			print render ( $content ['lvtypname'] );
+		} else {
+			print render ( $content ['typename'] );
+		}?>
+  <?php print render($content['code']); ?>
   <?php print render($content['ects']); ?>
   <?php print render($content['wst']); ?>
+  <?php print "<br>"; ?>
   <?php print render($content['verantname']); ?>
   <?php print render($content['verantemail']); ?>
-  <?php if( array_key_exists('lvtypname', $content)){
-  		 	print render($content['lvtypname']);
-  		}else{
- 			print render($content['typename']);
-		} 
-   ?>
-  <?php print render($content['lehrinhalte']); ?>
-  <?php print render($content['ziele']); ?>
+  <?php print "<br>"; ?>
+	
+	<?php  if (!empty($content['lehrinhalte'])) {
+  	?>
+  	<div class="field field-name-lehrinhalte field-type-text-long field-label-above">
+  	<div class="field-label">Lehrinhalte:</div>
+  	<div class="field-items"><div class="field-item even">
+  	<?= $node->lehrinhalte['und'][0]['value']?>
+  	</div></div></div>
+  	<?php }?>
   
+  <?php  if (!empty($content['ziele'])) {
+  	?>
+  	<div class="field field-name-ziele field-type-text-long field-label-above">
+  	<div class="field-label">Lehrveranstaltungsziele:</div>
+  	<div class="field-items"><div class="field-item even">
+  	<?= $node->ziele['und'][0]['value']?>
+  	</div></div></div>
+  <?php }?>
+  
+  <?php print "<br>"; ?>
+  <?php  if (!empty($content['voraussetzungen'])) {
+  	?>
+  	<div class="field field-name-voraussetzungen field-type-text-long field-label-above">
+  	<div class="field-label">Anmeldevoraussetzung(en)</div>
+  	<div class="field-items"><div class="field-item even">
+  	<?= $node->voraussetzungen['und'][0]['value']?>
+  	</div></div></div>
+  <?php }?>
+  
+  <?php print "<br>"; ?>
+  <?php  if (!empty($content['code'])) {
+  	?>
+  	<a href="http://lss.jku.at/studienhandbuch/<?= $node->code['und'][0]['value']?>"><b>Link zum Studienhandbuch</b></a>
+  <?php }?>
 </div>
